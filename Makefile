10e1e8c27eb17e8a825b7f13a889b5fc16d9b175
Tatu Peltola <tatu.peltola@gmail.com>
2020-06-11 22:50:02 +0300

    Add LED blinking code

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+softa.bin: softa.elf
+	arm-none-eabi-objcopy -O binary -S $^ $@
+
 softa.elf: *.c *.h *.s *.ld Makefile
-	arm-none-eabi-gcc -o $@ -lc -lm -lnosys -specs=nano.specs -T ./*.ld -g -mcpu=cortex-m7 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -DSTM32H745xx -DCORE_CM7 ./*.s ./*.c
+	arm-none-eabi-gcc -o $@ -lc -lm -lnosys -specs=nano.specs -T ./*.ld -g -mcpu=cortex-m7 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -DSTM32H745xx -DCORE_CM7 ./*.s ./*.c
--- softa.c
+++ softa.c
@@ -1,4 +1,24 @@
+#include "stm32h745xx.h"
+
+void SysTick_Handler()
+{
+	GPIOB->ODR ^= 1 | (1<<14);
+}
+
 void main(void)
 {
-	for(;;);
+	// Configure PB0 and PB14 as outputs
+	RCC->AHB4ENR |= RCC_AHB4ENR_GPIOBEN;
+	GPIOB->MODER = GPIO_MODER_MODE0_0 | GPIO_MODER_MODE14_0;
+
+	// Turn one LED on already
+	GPIOB->ODR = 1<<14;
+
+	// Use SysTick for periodic interrupts
+	SysTick_Config(10000000);
+	NVIC_EnableIRQ(SysTick_IRQn);
+
+	// Sleep outside of interrupts
+	for(;;)
+		__WFI();
 }
